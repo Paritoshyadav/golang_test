8c15248687e5c6b72fef01fa8a931c1fe7c897c5
Paritosh yadav <y.paritosh@gmail.com>
2021-08-07 19:07:26 +0530

    context written in idomatic way

--- context/context_test.go
+++ context/context_test.go
@@ -2,52 +2,92 @@ package main
 
 import (
 	"context"
+	"errors"
 	"net/http"
 	"net/http/httptest"
 	"testing"
 	"time"
 )
 
+type SpyResponseHeader struct {
+	written bool
+}
+
+func (s *SpyResponseHeader) Header() http.Header {
+	s.written = true
+	return nil
+
+}
+
+func (s *SpyResponseHeader) Write([]byte) (int, error) {
+	s.written = true
+	return 0, errors.New("not implemented")
+
+}
+
+func (s *SpyResponseHeader) WriteHeader(statusCode int) {
+	s.written = true
+
+}
+
 type Teststore struct {
-	response  string
-	cancelled bool
-	t         *testing.T
+	response string
+	t        *testing.T
 }
 
-func (store *Teststore) Fetch() string {
-	time.Sleep(100 * time.Millisecond)
-	return store.response
+func (store *Teststore) Fetch(ctx context.Context) (string, error) {
 
-}
+	data := make(chan string, 1)
 
-func (store *Teststore) Cancel() {
+	go func() {
+		var result string
+		for _, v := range store.response {
+			select {
+			case <-ctx.Done():
+				store.t.Log("got cancelled in between")
+				return
+			default:
+				time.Sleep(10 * time.Millisecond)
+				result += string(v)
 
-	store.cancelled = true
+			}
 
-}
+		}
+		data <- result
+	}()
 
-func (store *Teststore) assertNoCancel() {
-	store.t.Helper()
-	if store.cancelled {
-		store.t.Error("store was not told to cancel")
+	select {
+	case <-ctx.Done():
+		return "", ctx.Err()
+
+	case res := <-data:
+		return res, nil
 	}
 
 }
 
-func (store *Teststore) assertCancel() {
-	store.t.Helper()
-	if !store.cancelled {
-		store.t.Error("store was told to cancel but did not")
-	}
+// func (store *Teststore) assertNoCancel() {
+// 	store.t.Helper()
+// 	if store.cancelled {
+// 		store.t.Error("store was not told to cancel")
+// 	}
 
-}
+// }
+
+// func (store *Teststore) assertCancel() {
+// 	store.t.Helper()
+// 	if !store.cancelled {
+// 		store.t.Error("store was told to cancel but did not")
+// 	}
+
+// }
 
 func TestContext(t *testing.T) {
 	want := "testing"
 
 	t.Run("Getting correct response", func(t *testing.T) {
 
-		val := &Teststore{response: want, cancelled: false, t: t}
+		val := &Teststore{response: want, t: t}
 		s := Server(val)
 
 		request := httptest.NewRequest(http.MethodGet, "/", nil)
@@ -58,13 +98,12 @@ func TestContext(t *testing.T) {
 		if respone.Body.String() != want {
 			t.Errorf("want %s but get %s", want, respone.Body.String())
 		}
-		val.assertNoCancel()
 
 	})
 
 	t.Run("Cancel context", func(t *testing.T) {
 
-		val := &Teststore{response: want, cancelled: false, t: t}
+		val := &Teststore{response: want, t: t}
 		s := Server(val)
 
 		request := httptest.NewRequest(http.MethodGet, "/", nil)
@@ -72,11 +111,13 @@ func TestContext(t *testing.T) {
 		time.AfterFunc(5*time.Millisecond, cancel)
 		request = request.WithContext(requestctx)
 
-		respone := httptest.NewRecorder()
+		respone := &SpyResponseHeader{}
 
 		s.ServeHTTP(respone, request)
 
-		val.assertCancel()
+		if respone.written {
+			t.Errorf("should not be written")
+		}
 
 	})
 
--- context/main.go
+++ context/main.go
@@ -1,33 +1,23 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"net/http"
 )
 
 type Store interface {
-	Fetch() string
-	Cancel()
+	Fetch(ctx context.Context) (string, error)
 }
 
 func Server(store Store) http.HandlerFunc {
 
 	return func(rw http.ResponseWriter, r *http.Request) {
-
-		ctx := r.Context()
-		data := make(chan string, 1)
-		go func() {
-			data <- store.Fetch()
-
-		}()
-
-		select {
-		case d := <-data:
-			fmt.Fprint(rw, d)
-		case <-ctx.Done():
-			store.Cancel()
+		data, err := store.Fetch(r.Context())
+		if err != nil {
+			return
 		}
-
+		fmt.Fprint(rw, data)
 	}
 
 }
