b169a5e79a22baec7587b44f2b22cc69c7a0f01a
Paritosh yadav <y.paritosh@gmail.com>
2021-08-25 22:21:05 +0530

    test and function to get clock points

--- math/main.go
+++ math/main.go
@@ -1,14 +1,36 @@
 package main
 
-import "time"
+import (
+	"math"
+	"time"
+)
 
 type Point struct {
 	x float64
 	y float64
 }
 
-func SecondHand(tm time.Time) Point {
-	return Point{150, 60}
+//60 min = 360 , 1 min = 360/60 = 2*pi/60 = 2*pi/2*30=Pi/30
+func SecondHandInRadian(tm time.Time) float64 {
+	return (math.Pi / (30 / (float64(tm.Second()))))
+}
+
+func secondHandPoint(tm time.Time) Point {
+	angle := SecondHandInRadian(tm)
+	return Point{x: math.Sin(angle), y: math.Cos(angle)}
+}
+
+// 1) Scale it to the length of the hand
+// 2) Flip it over the X axis because to account for the SVG having an origin in
+// the top left hand corner
+// 3) Translate it to the right position (so that it's coming from an origin of
+// (150,150))
+func secondHand(tm time.Time) Point {
+	p := secondHandPoint(tm)
+	p = Point{x: p.x * 90, y: p.y * 90}   //scale
+	p = Point{x: p.x, y: -p.y}            //flip
+	p = Point{x: p.x + 150, y: p.y + 150} //translate
+	return p
 }
 
 func main() {
--- math/math_test.go
+++ math/math_test.go
@@ -1,17 +1,92 @@
 package main
 
 import (
+	"math"
 	"testing"
 	"time"
 )
 
-func TestMath(t *testing.T) {
+func TestSecondsInRadians(t *testing.T) {
 
-	tm := time.Date(1996, time.September, 14, 0, 0, 0, 0, time.UTC)
-	want := Point{x: 150, y: 150 - 90}
-	got := SecondHand(tm)
-	if got != want {
-		t.Errorf("got %v but want %v", got, want)
+	cases := []struct {
+		time  time.Time
+		angle float64
+	}{
+		{simpleTime(0, 0, 30), math.Pi},
+		{simpleTime(0, 0, 0), 0},
+		{simpleTime(0, 0, 45), (math.Pi / 2) * 3},
+		{simpleTime(0, 0, 7), (math.Pi / 30) * 7},
+	}
+
+	for _, c := range cases {
+
+		t.Run(testName(c.time), func(t *testing.T) {
+
+			got := SecondHandInRadian(c.time)
+
+			if got != c.angle {
+
+				t.Errorf("got %v but want %v", got, c.angle)
+
+			}
+
+		})
+
 	}
 
 }
+
+func TestSecondHandPoint(t *testing.T) {
+	cases := []struct {
+		time  time.Time
+		point Point
+	}{
+		{simpleTime(0, 0, 30), Point{0, -1}},
+		{simpleTime(0, 0, 45), Point{-1, 0}},
+	}
+	for _, c := range cases {
+		t.Run(testName(c.time), func(t *testing.T) {
+			got := secondHandPoint(c.time)
+			if !roughlyEqualPoint(got, c.point) {
+				t.Fatalf("Wanted %v Point, but got %v", c.point, got)
+			}
+		})
+	}
+
+}
+
+func TestSecondHandAt30Seconds(t *testing.T) {
+	tm := time.Date(1337, time.January, 1, 0, 0, 30, 0, time.UTC)
+
+	want := Point{x: 150, y: 150 + 90}
+	got := secondHand(tm)
+
+	if got != want {
+		t.Errorf("Got %v, wanted %v", got, want)
+	}
+}
+
+func simpleTime(hour, min, sec int) time.Time {
+	return time.Date(1996, time.September, 14, hour, min, sec, 0, time.UTC)
+
+}
+
+func testName(t time.Time) string {
+	return t.Format("15:04:05")
+}
+
+func roughltEqual(a, b float64) bool {
+	const equalityThreshold = 1e-7
+	return math.Abs(a-b) < equalityThreshold
+
+}
+
+func roughlyEqualPoint(a, b Point) bool {
+	return roughltEqual(a.x, b.x) &&
+		roughltEqual(a.y, b.y)
+}
+
+// want := Point{x: 150, y: 150 - 90}
+// got := SecondHand(cases.time)
+// if got != want {
+// 	t.Errorf("got %v but want %v", got, want)
